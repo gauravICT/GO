72d87af78c1664aa60c0ba5c9e4b566ad13b08d8
gauravICT <44467412+gauravICT@users.noreply.github.com>
2019-09-04 22:18:03 +0530

    Create main.go
    This is a simple hello program in go

---
+++ first_program/main.go
@@ -0,0 +1,7 @@
+package main                          // name of the package this file belongs to
+
+import "fmt"
+
+func main() {                       // a special func that tells go where to start
+	fmt.Println("HELLO GOPHERS")
+}
